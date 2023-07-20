d8338e4569418e4b3878676ad6ff6ae31ef50be1
FelixHarris <felhar75@kingshigh.school.nz>
2023-07-20 13:26:23 +1200

    Got w,a,s,d working and can interact with objects

--- WalkingSim/Player.gd
+++ WalkingSim/Player.gd
@@ -8,6 +8,13 @@ extends CharacterBody3D
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var relics_destroyed = 0
+
+@onready var ray = $Camera3D/RayCast3D
+@onready var interaction_notifier = $Control/InteractionNotifier
+@onready var collection_tracker = $Control/MarginContainer/CollectionTracker
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -23,10 +30,21 @@ func _unhandled_input(event):
 		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.2,1.2)
 
 
+func check_ray_hit():
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider and ray.get_collider().is_in_group("Destroy"):
+			interaction_notifier.visible = true
+		if Input.is_action_just_pressed("use"):
+			ray.get_collider().queue_free()
+			relics_destroyed += 1
+	else:
+		interaction_notifier.visible = false
+
 
 func _physics_process(delta):
 	# Add the gravity.
-	
+	check_ray_hit()
 	if Input.is_action_pressed("run"):
 		SPEED = run_speed
 	else:
@@ -44,7 +62,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = move_toward(velocity.x, direction.x * SPEED,FRICTION)    
