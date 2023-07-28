dcfb66a4fe6b651d1d2b39a40a5935fa3c2992a1
FelixHarris <felhar75@kingshigh.school.nz>
2023-07-28 13:26:09 +1200

    Jumpscare almost finished

--- WalkingSim/JumpScareTrigger.gd
+++ WalkingSim/JumpScareTrigger.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+var jump_scare_entity = preload("res://jumpscare.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +14,8 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	
+	if body.is_in_group("Player"):
+		print("Create the Jumpscare")
+		var jumpscare = jump_scare_entity.instantiate()
+		jumpscare.global_transform.origin = $Marker3D.global_transform.origin
+		add_child(jumpscare)
---
+++ WalkingSim/jumpscare.gd
@@ -0,0 +1,12 @@
+extends Sprite3D
+
+var move_speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(Vector3.FORWARD * delta * move_speed)
