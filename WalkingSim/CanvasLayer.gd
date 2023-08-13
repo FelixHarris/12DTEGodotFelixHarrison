1d42b932f212f13967e577212b0fa138069dc339
FelixHarris <100237106+FelixHarris@users.noreply.github.com>
2023-08-13 21:18:16 +1200

    Doing main menu

--- WalkingSim/CanvasLayer.gd
+++ WalkingSim/CanvasLayer.gd
@@ -1,12 +1,27 @@
 extends CanvasLayer
 
-@onready var mai:
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var main: Control = $Main
+@onready var settings: Control = $Settings
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_button_play_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_button_settings_pressed():
+	main.visible = false
+	settings.visible = true
+
+
+
+func _on_button_quit_pressed():
+	get_tree().quit()
+
+
+
+
+
+func _on_button_settings_back_pressed():
+	main.visible = true
+	settings.visible = false
