4002e32369a333e7e7b8090c8294f2883208b034
FelixHarris <felhar75@kingshigh.school.nz>
2023-09-05 09:58:38 +1200

    Enemy walking fixed

--- WalkingSim/Enemy.gd
+++ WalkingSim/Enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @onready var nav_agent = $NavigationAgent3D
 @onready var player = get_node("/root/World/Player")
 
-var movement_speed = 1
+var movement_speed = 2
 
 func _ready():
 	call_deferred("get_target")
@@ -11,7 +11,8 @@ func _ready():
 	
 func get_target():
 	nav_agent.target_position = player.global_transform.origin
-	movement_speed *= 1.0005
+	movement_speed += .1
+	print(movement_speed)
 	
 func _physics_process(delta):
 	var direction_to_player = (player.global_transform.origin - global_transform.origin).normalized()
@@ -21,7 +22,6 @@ func _physics_process(delta):
 	#rotation_degrees.z = 0
 	if nav_agent.is_navigation_finished():
 		get_target()
-	print(movement_speed)
 		
 	var current_position = global_position
 	var next_position = nav_agent.get_next_path_position()
@@ -31,3 +31,7 @@ func _physics_process(delta):
 	velocity = new_velocity
 	move_and_slide()
 	
+
+
+func _on_timer_timeout():
+	get_target()
